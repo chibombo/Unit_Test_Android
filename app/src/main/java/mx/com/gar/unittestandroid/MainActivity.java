affcf0789ebddf787541f2f0fc29b5d570d72730
Luis Genaro Arvizu Vega <genaro.arvizu95@gmail.com>
2018-12-12 09:07:47 -0600

    Button Added

--- app/src/main/java/mx/com/gar/unittestandroid/MainActivity.java
+++ app/src/main/java/mx/com/gar/unittestandroid/MainActivity.java
@@ -1,13 +1,31 @@
 package mx.com.gar.unittestandroid;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button btnHelloWorld;
+    final Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnHelloWorld = (Button)findViewById(R.id.btnHelloWorld);
+        btnHelloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage("Hello Word!").setTitle("Example");
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
     }
+
 }
